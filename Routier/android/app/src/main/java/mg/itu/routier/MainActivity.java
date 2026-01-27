e61686fdeb32f6817e4a67ae8e7cfc93345f17e9
ANDRIAMALALA Rivo Ny Aina Tojonirina <tojoandriam238@gmail.com>
2026-01-28 00:39:10 +0300

    depl success

---
+++ Routier/android/app/src/main/java/mg/itu/routier/MainActivity.java
@@ -0,0 +1,5 @@
+package mg.itu.routier;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- Routier/capacitor.config.ts
+++ Routier/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'mg.itu.routier',
   appName: 'Routier',
   webDir: 'dist'
 };
